3c0c0b42228cb38a42a08bdaa0495034e97ec236
Robey Pointer <robeypointer@gmail.com>
2016-04-16 18:56:53 -0700

    playing with tokens (from a few weeks ago). gonna try something different, so saving this.

---
+++ test/src/test_tokens.js
@@ -0,0 +1,117 @@
+"use strict";
+
+import packrattle from "../../lib";
+
+import "should";
+import "source-map-support/register";
+
+// experiments for token parsing.
+
+class Token {
+  constructor(name, span, value) {
+    this.name = name;
+    this.span = span;
+    this.value = value;
+  }
+
+  toString() {
+    if (!this.value) return this.name.toUpperCase();
+    return this.name.toUpperCase() + "(" + this.value + ")";
+  }
+}
+
+function makeLexer(f) {
+  function token(name, matcher, f) {
+    name = name.toUpperCase();
+    if (!matcher) matcher = name;
+
+    return packrattle(matcher).onMatch((m, span) => {
+      const value = f ? f(m) : null;
+      return new Token(name, span, value);
+    });
+  }
+
+  function drop(matcher) {
+    return packrattle(matcher).drop();
+  }
+
+  const parsers = f(token, drop).map(p => {
+    if (typeof p == "string") return token(p);
+    return p;
+  });
+  return packrattle.repeat(packrattle.alt(...parsers));
+}
+
+// -----
+
+const lexer = makeLexer((token, drop) => {
+  return [
+    drop(/\s+/),
+    token("number", /\d+/, m => parseInt(m[0], 10)),
+    "(",
+    ")",
+    "+",
+    "-",
+    "*",
+    "/",
+    "%"
+  ];
+});
+
+
+function token(name) {
+  return packrattle.newParser(name, { cacheable: true }, (state, results) => {
+    name = name.toUpperCase();
+    if (state.pos >= state.text.length) return results.add(state.failure());
+    const currentToken = state.text[state.pos];
+    console.log("check:", currentToken);
+    results.add(currentToken.name == name ? state.advance(1).success(currentToken) : state.failure());
+  });
+}
+
+
+/*
+ * create a new Parser object:
+ *   - name: type of parser, in one word ("alt", "optional", ...)
+ *   - options:
+ *       - children: list of nested parsers, if this is a combiner
+ *       - describe: `(children: Array(String)) => String`: returns a
+ *         description of the parser for debugging, including children, like
+ *         "x or y or z"
+ *   - matcher: `(state, results, ...children) => void`
+ *       - state: `ParserState`: current text and position
+ *       - results: `ResultSet`: container for eventual result (success or
+ *         failure)
+ *       - children: same as `options.children`, but with implicits resolved
+ */
+
+describe("token parser", () => {
+  const parens = packrattle([ token("(").drop(), () => expr, token(")").drop() ]);
+  const atom = packrattle.alt(token("number"), parens);
+  const term = packrattle.reduce(atom, packrattle.alt(token("*"), token("/"), token("%")), {
+    first: x => x,
+    next: (left, op, right) => ({ op, left, right })
+  });
+  const expr = packrattle.reduce(term, packrattle.alt(token("+"), token("-")), {
+    first: x => x,
+    next: (left, op ,right) => ({ op, left, right })
+  });
+
+
+  it("lexes", () => {
+    const tokenstream = lexer.run("1 + (2 + 3) * 4");
+    tokenstream.join(" ").should.eql("NUMBER(1) + ( NUMBER(2) + NUMBER(3) ) * NUMBER(4)");
+  });
+
+  it("parses", () => {
+    const tokenstream = lexer.run("1 + 2");
+    tokenstream.join(" ").should.eql("NUMBER(1) + NUMBER(2)");
+    console.log(expr.run(tokenstream, { debugger: console.log }));
+    try {
+      expr.run(lexer.run("1 + "), { debugger: console.log });
+    } catch (error) {
+      console.log(error);
+      console.log(error.span.toSquiggles().join("\n"));
+    }
+  });
+});
