ee54fc0f9faf50ce93d78a9d949230fdef035253
Robey Pointer <robeypointer@gmail.com>
2017-06-16 10:05:12 -0700

    export new functions and tokenizer.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-export { Match, MatchFailure, MatchSuccess, Span } from "./matcher";
+export { EngineOptions } from "./engine";
+export { Match, MatchFailure, MatchSuccess, mergeSpan, Span } from "./matcher";
 export { PriorityQueue } from "./priority_queue";
 export { PromiseSet } from "./promise_set";
 export { Line, SourceSpan } from "./source_span";
+export { makeTokenizer, matchToken, Token, TokenRegexRule, TokenRules } from "./tokenizer";
 
 // need to do in two parts, so tsc understands that the simple/combo parsers below are of known type.
 import { LazyParser, Parser, parser } from "./parser";
@@ -16,4 +18,5 @@ const succeed = simple.succeed;
 export { end, matchRegex, matchString, reject, succeed };
 
 export { alt, chain, check, not, optional, optionalOr, repeat, RepeatOptions, seq } from "./combiners";
+export { seq2, seq3, seq4, seq5 } from "./combiners";
 export { reduce, ReduceOptions, repeatIgnore, repeatSeparated, seqIgnore } from "./convenience";
